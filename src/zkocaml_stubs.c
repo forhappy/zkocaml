4b37886bd542bd2a266d04c33809517668b6a581
forhappy <haipingf@gmail.com>
2013-11-24 13:16:30 +0800

    update zkocaml_stubs.c

--- src/zkocaml_stubs.c
+++ src/zkocaml_stubs.c
@@ -58,9 +58,44 @@ static struct custom_operations zhandle_struct_ops = {
   custom_deserialize_default
 };
 
+/**
+ * Create a handle to used communicate with zookeeper.
+ *
+ * This method creates a new handle and a zookeeper session that corresponds
+ * to that handle. Session establishment is asynchronous, meaning that the
+ * session should not be considered established until (and unless) an
+ * event of state ZOO_CONNECTED_STATE is received.
+ *
+ * @host Comma separated host:port pairs, each corresponding to a zk
+ *   server. e.g. "127.0.0.1:3000,127.0.0.1:3001,127.0.0.1:3002"
+ *
+ * @watcher_fn The global watcher callback function. When notifications are
+ *   triggered this function will be invoked.
+ *
+ * @clientid The id of a previously established session that this
+ *   client will be reconnecting to. Pass 0 if not reconnecting to a previous
+ *   session. Clients can access the session id of an established, valid,
+ *   connection by calling zoo_client_id. If the session corresponding to
+ *   the specified clientid has expired, or if the clientid is invalid for 
+ *   any reason, the returned zhandle_t will be invalid -- the zhandle_t
+ *   state will indicate the reason for failure (typically
+ *   ZOO_EXPIRED_SESSION_STATE).
+ *
+ * @context The handback object that will be associated with this instance 
+ *   of zhandle_t. Application can access it (for example, in the watcher
+ *   callback) using zoo_get_context. The object is not used by zookeeper
+ *   internally and can be null.
+ *
+ * @flags Reserved for future use. Should be set to zero.
+ *
+ * @return A pointer to the opaque zhandle structure. If it fails to create
+ * a new zhandle the function returns NULL and the errno variable indicates
+ * the reason.
+ */
+
 CAMLprim value
 zkocaml_init_native(value host, value watcher_fn, value recv_timeout,
-             value clientid, value context, value flag)
+                    value clientid, value context, value flag)
 {
   CAMLparam1(host);
 }
@@ -69,5 +104,261 @@ CAMLprim value
 zkocaml_init_bytecode(value *argv, int argn)
 {
   return zkocaml_init_native(argv[0], argv[1], argv[2],
-          argv[3], argv[4], argv[5]);
+                             argv[3], argv[4], argv[5]);
 }
+
+CAMLprim value
+zkocaml_close(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_client_id(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_recv_timeout(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_get_context(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_set_context(value zh, value context)
+{
+  CAMLparam1(zh);
+}
+
+
+CAMLprim value
+zkocaml_set_watcher(value zh, value watcher_fn)
+{
+  CAMLparam1(zh);
+}
+
+
+CAMLprim value
+zkocaml_get_connected_host(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_state(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_acreate(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_adelete(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_aexists(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_awexists(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_aget(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_awget(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_aset(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_aget_children(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_awget_children(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_aget_children2(value zh)
+{
+  CAMLparam1(zh);
+}
+
+
+CAMLprim value
+zkocaml_awget_children2(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_async(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_aget_acl(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_aset_acl(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_zerror(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_add_auth(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_is_unrecoverable(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_set_debug_level(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_set_log_stream(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_deterministic_conn_order(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_create(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_delete(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_exists(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_wexists(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_get(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_wget(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_set(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_set2(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_get_children(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_wget_children(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_get_children2(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_wget_children2(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_get_acl(value zh)
+{
+  CAMLparam1(zh);
+}
+
+CAMLprim value
+zkocaml_set_acl(value zh)
+{
+  CAMLparam1(zh);
+}
+
